f057a2611050ddbaab36bccced1ec559ba939a63
Stefan Junker <mail@stefanjunker.de>
2018-12-12 12:13:35 +0100

    examples/image: add experimental ScopeTracker allocator

--- examples/image.rs
+++ examples/image.rs
@@ -6,9 +6,13 @@ extern crate tokio;
 
 use dkregistry::{reference, render};
 use futures::prelude::*;
+use scopetracker::{AllocationTracker, ScopeTracker};
 use std::result::Result;
 use std::str::FromStr;
 use std::{boxed, env, error, fs, io};
+
+#[global_allocator]
+static GLOBAL: AllocationTracker = AllocationTracker::new();
 
 mod common;
 
@@ -138,4 +142,92 @@ fn run(
     let r = render::unpack(&blobs, &can_path).unwrap();
 
     Ok(r)
+}
+
+mod scopetracker {
+    use super::GLOBAL;
+
+    use std::alloc::{GlobalAlloc, Layout, System};
+    use std::sync::atomic::{AtomicIsize, Ordering};
+
+    pub struct AllocationTracker {
+        mem: AtomicIsize,
+    }
+
+    impl AllocationTracker {
+        pub const fn new() -> Self {
+            AllocationTracker {
+                mem: AtomicIsize::new(0),
+            }
+        }
+
+        fn current_mem(&self) -> isize {
+            self.mem.load(Ordering::SeqCst)
+        }
+    }
+
+    unsafe impl GlobalAlloc for AllocationTracker {
+        unsafe fn alloc(&self, layout: Layout) -> *mut u8 {
+            self.mem.fetch_add(layout.size() as isize, Ordering::SeqCst);
+            System.alloc(layout)
+        }
+
+        unsafe fn dealloc(&self, ptr: *mut u8, layout: Layout) {
+            self.mem.fetch_sub(layout.size() as isize, Ordering::SeqCst);
+            System.dealloc(ptr, layout)
+        }
+    }
+
+    pub struct ScopeTracker<'a> {
+        at_start: isize,
+        name: &'a str,
+        file: &'static str,
+        line: u32,
+    }
+
+    impl<'a> ScopeTracker<'a> {
+        pub fn new(name: &'a str, file: &'static str, line: u32) -> Self {
+            Self {
+                at_start: GLOBAL.current_mem(),
+                name,
+                file,
+                line,
+            }
+        }
+    }
+
+    impl Drop for ScopeTracker<'_> {
+        fn drop(&mut self) {
+            let old = self.at_start;
+            let new = GLOBAL.current_mem();
+            if old != new {
+                if self.name == "" {
+                    println!(
+                        "{}:{}: {} bytes escape scope",
+                        self.file,
+                        self.line,
+                        new - old
+                    );
+                } else {
+                    println!(
+                        "{}:{} '{}': {} bytes escape scope",
+                        self.file,
+                        self.line,
+                        self.name,
+                        new - old
+                    );
+                }
+            }
+        }
+    }
+
+    #[macro_export]
+    macro_rules! mem_guard {
+        () => {
+            mem_guard!("")
+        };
+        ($e:expr) => {
+            let _guard = ScopeTracker::new($e, file!(), line!());
+        };
+    }
 }
